d2f878ae0698f384ea4b84c39084e2e2d9dcfbed
yiju_wu <yiju_wu@brogent.com>
2024-05-11 21:22:41 +0800

    Add helloworld.c

---
+++ the-c-programming-language/helloworld.c
@@ -0,0 +1,19 @@
+// -*- coding: utf-8 -*-
+/*
+Created on Sat 11 May 2024
+
+Author:  yiju_wu@brogent.com
+
+File: helloworld.c
+
+Topic:
+
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, world.\n");
+
+    return 0;
+}
